b7408de9f70849eb1a97bf5d9515e6bf4106881b
Damjan Stojanovski <damjan.stojanovski123@gmail.com>
2022-03-19 15:41:36 +0100

    Implemented counting functionality for the product and the overall price

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const burgerMenu = document.querySelector('.burgerMenu');
 const sideMenuMobile = document.querySelector('.mobile_menu_hide');
 const closeButtonMobile = document.querySelector('.closeButtonMobile');
+const buttonNext = document.querySelector('.next');
+const buttonPrevious = document.querySelector('.previous');
+
+const plusProduct = document.querySelector('.plus')
+const minusProduct = document.querySelector('.minus')
+const result = document.querySelector('.result')
+const priceShoes = document.querySelector('.price')
+
+let count = 1; 
+let price = 125; 
+
 
 burgerMenu.addEventListener('click',() => {
 
@@ -12,4 +23,27 @@ burgerMenu.addEventListener('click',() => {
 closeButtonMobile.addEventListener('click', () =>{
     sideMenuMobile.classList.remove('mobile_menu');
 })
+
+plusProduct.addEventListener('click',() => {
+    count += 1;
+        result.innerHTML = count;
+
+        if(result.innerHTML > 1) { 
+            let newPrice = price += 125; 
+            priceShoes.innerHTML = `$${newPrice}.00`;
+        }
+
+
+})
+
+minusProduct.addEventListener('click',() => {
+    if(result.innerHTML > 1) { 
+    count -= 1;
+        result.innerHTML = count;
+        let priceDecrease = price -= 125;
+        priceShoes.innerHTML = `$${priceDecrease}.00`
+    }
+        
+})
+
 
