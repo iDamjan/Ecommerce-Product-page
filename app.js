df0a705f214525efecfcae9abd0f59271ed56908
Damjan Stojanovski <damjan.stojanovski123@gmail.com>
2022-03-30 18:36:15 +0200

    Pull reaquest changes

--- app.js
+++ app.js
@@ -1,44 +1,8 @@
-const burgerMenu = document.querySelector(".burgerMenu");
-const grayedOutBody = document.querySelector(".grayedOut_body");
-const sideMenuMobile = document.querySelector(".mobile_menu_hide");
-const closeButtonMobile = document.querySelector(".closeButtonMobile");
-const buttonNext = document.querySelector(".next");
-const buttonPrevious = document.querySelector(".previous");
-
-const plusProduct = document.querySelector(".plus");
-const minusProduct = document.querySelector(".minus");
-const result = document.querySelector(".result");
-const priceShoes = document.querySelector(".price");
-const carosel = document.querySelectorAll(".carosel img");
-
-const addToCartButton = document.querySelector(".add_toCart");
-const productInBasket = document.querySelector(".product_added_hide");
-const emptyBasketText = document.querySelector(".cart_empty_text");
-
-const totalPriceBasket = document.querySelector(".total_price");
-const quantity = document.querySelector(".quantity");
-const iconDelete = document.querySelector(".icon_delete");
-
-const imagesCount = carosel.length;
-
-const desktopImagesThumbnail = document.querySelectorAll(".little_images img");
-const desktopCarousel = document.querySelector(".desktopCarousel_hide");
-const desktopDefaultProductImage = document.querySelector(
-  ".product_image_default_desktop"
-);
-const grayedOutBodyDesktop = document.querySelector(".grayedOut_body");
-const closeButtonDesktop = document.querySelector(".closeButton");
-const previousButtonDesktop = document.querySelector(".previousButtonDesktop");
-const nextButtonDesktop = document.querySelector(".nextButtonDesktop");
-
-const cartImage = document.querySelector(".cart");
-const cartCounter = document.querySelector(".itemsCountOnCart_hide");
-const cartCounterValue = document.querySelector(".cart_counter");
 
 let count = 1;
 let price = 125;
 const localStorageCount = localStorage.getItem("itemsInCart");
-console.log(localStorageCount);
+
 
 if (localStorageCount) {
   productInBasket.classList.add("product_added");
@@ -50,7 +14,7 @@ if (localStorageCount) {
   cartCounterValue.innerHTML = localStorageCount;
 }
 
-//Mobile Carousel
+// Mobile Carousel
 const changeMainImage = (currentImgId, currentImg) => {
   carosel[currentImgId].classList.add("product_image_default");
   carosel[currentImgId].classList.remove("product_image");
@@ -58,7 +22,7 @@ const changeMainImage = (currentImgId, currentImg) => {
   currentImg.classList.add("product_image");
 };
 
-//button next
+// button next
 buttonNext.addEventListener("click", (e) => {
   const currentImg = document.querySelector(".product_image_default");
   let currentImgId = parseInt(currentImg.id) + 1;
@@ -69,7 +33,7 @@ buttonNext.addEventListener("click", (e) => {
   changeMainImage(currentImgId, currentImg);
 });
 
-//button previous
+// button previous
 buttonPrevious.addEventListener("click", () => {
   const currentImg = document.querySelector(".product_image_default");
   let previousImgId = parseInt(currentImg.id) - 1;
@@ -80,9 +44,8 @@ buttonPrevious.addEventListener("click", () => {
   changeMainImage(previousImgId, currentImg);
 });
 
-//Mobile product container
-const avatar = document.querySelector(".avatar");
-const cartContainer = document.querySelector(".mobile_cart_container_hide");
+// Mobile product container
+
 
 avatar.addEventListener("click", () => {
   cartContainer.classList.toggle("mobile_cart_container");
@@ -91,11 +54,11 @@ avatar.addEventListener("click", () => {
 cartImage.addEventListener("click", () => {
   cartContainer.classList.toggle("mobile_cart_container");
 });
-//Mobile product container
+// Mobile product container
 
-//Desktop product container
+// Desktop product container
 
-//Adding products in the basket
+// Adding products in the basket
 addToCartButton.addEventListener("click", () => {
   productInBasket.classList.add("product_added");
   emptyBasketText.style.display = "none";
@@ -103,16 +66,16 @@ addToCartButton.addEventListener("click", () => {
   localStorage.setItem("itemsInCart", count);
 });
 
-//Delete the product from the basket
+// Delete the product from the basket
 iconDelete.addEventListener("click", () => {
   productInBasket.classList.remove("product_added");
   emptyBasketText.style.display = "flex";
   cartCounter.classList.remove("itemsCountOnCart");
   localStorage.clear();
 });
-//Desktop image selection
+// Desktop image selection
 
-//Openning and closing the mobile menu
+// Openning and closing the mobile menu
 burgerMenu.addEventListener("click", () => {
   sideMenuMobile.classList.add("mobile_menu");
   grayedOutBody.style.display = "block";
@@ -122,9 +85,9 @@ closeButtonMobile.addEventListener("click", () => {
   sideMenuMobile.classList.remove("mobile_menu");
   grayedOutBody.style.display = "none";
 });
-//Openning and closing the mobile menu
+// Openning and closing the mobile menu
 
-//Counting of the product
+// Counting of the product
 plusProduct.addEventListener("click", () => {
   count += 1;
   result.innerHTML = count;
@@ -148,9 +111,9 @@ minusProduct.addEventListener("click", () => {
     totalPriceBasket.innerHTML = ` $${priceDecrease}.00`;
   }
 });
-//Counting of the product
+// Counting of the product
 
-//desktop carousel
+// Desktop carousel
 
 desktopImagesThumbnail.forEach((img) => {
   img.addEventListener("click", (e) => {
@@ -177,7 +140,7 @@ closeButtonDesktop.addEventListener("click", () => {
 });
 
 // Light box gallery
-const lightboxImages = document.querySelectorAll(".lightBoxImages");
+
 
 previousButtonDesktop.addEventListener("click", () => {
   const currentImg = document.querySelector(".product_image_desktop_default");
---
+++ css-selectors.js
@@ -0,0 +1,40 @@
+
+
+const burgerMenu = document.querySelector(".burgerMenu");
+const grayedOutBody = document.querySelector(".grayedOut_body");
+const sideMenuMobile = document.querySelector(".mobile_menu_hide");
+const closeButtonMobile = document.querySelector(".closeButtonMobile");
+const buttonNext = document.querySelector(".next");
+const buttonPrevious = document.querySelector(".previous");
+
+const plusProduct = document.querySelector(".plus");
+const minusProduct = document.querySelector(".minus");
+const result = document.querySelector(".result");
+const priceShoes = document.querySelector(".price");
+const carosel = document.querySelectorAll(".carosel img");
+
+const addToCartButton = document.querySelector(".add_toCart");
+const productInBasket = document.querySelector(".product_added_hide");
+const emptyBasketText = document.querySelector(".cart_empty_text");
+
+const totalPriceBasket = document.querySelector(".total_price");
+const quantity = document.querySelector(".quantity");
+const iconDelete = document.querySelector(".icon_delete");
+
+const imagesCount = carosel.length;
+const desktopImagesThumbnail = document.querySelectorAll(".little_images img");
+const desktopCarousel = document.querySelector(".desktopCarousel_hide");
+const desktopDefaultProductImage = document.querySelector(
+  ".product_image_default_desktop"
+);
+const grayedOutBodyDesktop = document.querySelector(".grayedOut_body");
+const closeButtonDesktop = document.querySelector(".closeButton");
+const previousButtonDesktop = document.querySelector(".previousButtonDesktop");
+const nextButtonDesktop = document.querySelector(".nextButtonDesktop");
+
+const cartImage = document.querySelector(".cart");
+const cartCounter = document.querySelector(".itemsCountOnCart_hide");
+const cartCounterValue = document.querySelector(".cart_counter");
+const avatar = document.querySelector(".avatar");
+const cartContainer = document.querySelector(".mobile_cart_container_hide");
+const lightboxImages = document.querySelectorAll(".lightBoxImages");
